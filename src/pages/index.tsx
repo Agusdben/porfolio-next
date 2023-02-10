d41e3582cc51ae5d628142b66cc1d94b5ccc2685
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-02-10 18:36:50 -0300

    Creating i18n 🚀

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
 import AppLayout from '@/components/AppLayout'
+import Projects from '@/components/Projects'
+import { Project } from '@/types'
+import { GetStaticProps } from 'next'
 
-export default function Home () {
-  return <AppLayout></AppLayout>
+interface Props {
+  navbar: { [key: string]: string }
+  projects: Project[]
+}
+
+export default function Home ({ navbar, projects }: Props) {
+  return (
+    <AppLayout>
+      <Projects projects={projects} title={navbar.projects} />
+    </AppLayout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { locale } = context
+  const localeToUse = locale || 'es'
+
+  const projects = await import(`../locales/${localeToUse}/projects.ts`)
+  const navbar = await import(`../locales/${localeToUse}/navbar.ts`)
+
+  return {
+    props: {
+      projects: projects.default,
+      navbar: navbar.default
+    }
+  }
 }

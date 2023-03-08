ae425df5036c9dbe63bb56181a1b4aa243ad40d3
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-03-08 12:00:09 -0300

    changed repository

--- src/locales/es/projects.ts
+++ src/locales/es/projects.ts
@@ -24,7 +24,7 @@ const PROJECTS: Project[] = [
     repositories: [
       {
         type: 'Frontend',
-        url: 'https://github.com/Agusdben/Todos-frontend'
+        url: 'https://github.com/Agusdben/Todo-frontend-remake'
       },
       {
         type: 'Backend',
